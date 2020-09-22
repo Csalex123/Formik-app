f795ae66e50a4b470bbebb2fccc1fd9851af822f
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-21 22:16:19 -0300

    Adicinado o  yup para fazer validações

--- src/App.js
+++ src/App.js
@@ -8,20 +8,37 @@ function App() {
         console.log('SUBMIT', values);
     }
 
+    function validate(values) {
+        const erros = {};
+
+        if (!values.name) {
+            erros.name = 'Name é obrigatório';
+        }
+
+        if (!values.email) {
+            erros.email = 'Email é obrigatório';
+        }
+
+        return erros;
+    }
+
     return (
         <div>
             <Formik
+                validate={validate}
                 onSubmit={onSubmit}
                 initialValues={{ name: '', email: '' }}
-                render={({ values, handleSubmit }) => (
+                render={({ values }) => (
                     <Form>
                         <div>
                             <label>Nome:</label>
                             <Field name="name" type="text" />
+                       
                         </div>
                         <div>
                             <label>Email:</label>
                             <Field name="email" type="email" />
+                  
                         </div>
                         <button type="submit">Enviar</button>
                     </Form>
