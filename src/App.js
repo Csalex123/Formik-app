817d12ab57ed2f51670da94f62732cba52c18771
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-21 22:29:34 -0300

    Mensagens de erros

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import schema from './schema';
 
 
 function App() {
@@ -8,39 +9,25 @@ function App() {
         console.log('SUBMIT', values);
     }
 
-    function validate(values) {
-        const erros = {};
-
-        if (!values.name) {
-            erros.name = 'Name é obrigatório';
-        }
-
-        if (!values.email) {
-            erros.email = 'Email é obrigatório';
-        }
-
-        return erros;
-    }
-
     return (
         <div>
             <Formik
-                validate={validate}
+                validationSchema={schema}
                 onSubmit={onSubmit}
                 initialValues={{ name: '', email: '' }}
-                render={({ values }) => (
+                render={({ values, errors, touched, isValid }) => (
                     <Form>
                         <div>
                             <label>Nome:</label>
                             <Field name="name" type="text" />
-                       
+                            <ErrorMessage style={{ color: 'red' }} name="name" />
                         </div>
                         <div>
                             <label>Email:</label>
                             <Field name="email" type="email" />
-                  
+                            <ErrorMessage name="email" />
                         </div>
-                        <button type="submit">Enviar</button>
+                        <button type="submit" disabled={isValid}>Enviar</button>
                     </Form>
                 )} />
         </div>
